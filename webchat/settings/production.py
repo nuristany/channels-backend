e01730fbca703b1fa20db7e2072bc96c2946f900
Qais <nuristanyqais@gmail.com>
2025-04-23 21:02:47 +0430

    added csrf trusted origins

--- webchat/settings/production.py
+++ webchat/settings/production.py
@@ -3,3 +3,4 @@ from .common import *
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = ['channels-backend-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://channels-backend-production.up.railway.app']
