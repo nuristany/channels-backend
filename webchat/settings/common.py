a7f759f7ee282b35f437e0ac80223476884a00ee
Qais <nuristanyqais@gmail.com>
2025-04-27 11:18:08 +0430

    changed default permission classes

--- webchat/settings/common.py
+++ webchat/settings/common.py
@@ -143,9 +143,9 @@ REST_FRAMEWORK = {
 
     ),
 
-    # 'DEFAULT_PERMISSION_CLASSES' : [
-    #     'rest_framework.permissions.IsAuthenticated'
-    # ]
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticated'
+    ]
 }
 
 SIMPLE_JWT = {
