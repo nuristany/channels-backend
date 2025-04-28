8b06476e596fb9ac684a430e1ab237e9bf96e28a
Qais <nuristanyqais@gmail.com>
2025-04-28 07:38:58 +0430

    changed cors allowed origin

--- webchat/settings/common.py
+++ webchat/settings/common.py
@@ -35,7 +35,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Allow requests from your React app
     "http://192.168.1.106:5173",  # Allow requests from other devices on the same network
-   
+    "https://channels-backend-production.up.railway.app",
 ]
 
 
