e30fa8498b013c4fbf45019c6d1770e3a6ac819c
Qais <nuristanyqais@gmail.com>
2025-04-28 07:45:32 +0430

    added ssl header

--- webchat/settings/common.py
+++ webchat/settings/common.py
@@ -184,3 +184,6 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
+
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
--- webchat/settings/development.py
+++ webchat/settings/development.py
@@ -4,3 +4,4 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # No need to redefine DATABASES — already loaded from common.py
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
